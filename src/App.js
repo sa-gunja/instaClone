d257f46aff4a84585b18f6635bd6d7ef32119c2a
byhj0309 <byhj0309@gmail.com>
2021-04-04 18:07:59 +0900

    로그인 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Switch } from "react-router-dom";
 import Home from "pages/Home.js";
+import Login from "pages/Login.js";
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route path="/" component={Home} exact />
+        <Route path="/" component={Login} exact />
+        <Route path="/home" component={Home} exact />
       </Switch>
     </div>
   );

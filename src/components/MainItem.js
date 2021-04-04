2ba39730f3dbae1f0d8035e8ba24a277a4dc7e62
byhj0309 <byhj0309@gmail.com>
2021-04-04 17:28:03 +0900

    메인아이템

--- src/components/MainItem.js
+++ src/components/MainItem.js
@@ -1,11 +1,14 @@
 import styled from "styled-components";
 import SwiperCore, { Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from "swiper/react";
-import { useState, useEffect } from "react";
 
 import 'swiper/components/pagination/pagination.scss';
 
 SwiperCore.use([Pagination]);
+
+const Userheader = styled.div`
+  
+`;
 
 
 function MainItem(props) {
@@ -13,19 +16,18 @@ function MainItem(props) {
   return (
     <div>
       <div>
-        <div>img</div>
-        <div>{users.name}</div>
+        <div className="userName">img</div>
+        <div className="">{users.name}</div>
         <div>{users.follwState || "팔로우"}</div>
         <div>...</div>
       </div>
       <div>
-      <Swiper spaceBetween={0} slidesPerView={1} pagination={{ clickable: true }}>
-        <SwiperSlide>
-          d
-        </SwiperSlide>
+      <Swiper spaceBetween={0} slidesPerView={1} pagination>
+        {
+          users.imgs.map(item => <SwiperSlide><img src={item} alt="userImg" /></SwiperSlide>)
+        }
       </Swiper>
       </div>
-
     </div>
   );
 }
